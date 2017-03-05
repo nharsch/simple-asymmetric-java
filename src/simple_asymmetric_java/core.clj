85d6f8d8ced6bfc35fc1680aa78753941f946fc7
Nigel Harsch <nigelharsch@gmail.com>
2017-03-05 12:41:48 -0500

    stubs

--- src/simple_asymmetric_java/core.clj
+++ src/simple_asymmetric_java/core.clj
@@ -1,22 +1,54 @@
 (ns simple-asymmetric-java.core
-  (:require [caesium.crypto.secretbox :as sb]
-  [caesium.crypto.sign :as sign]
+  (:require 
+    [caesium.crypto.secretbox :as sb]
+    [caesium.crypto.sign :as sign]
+    [caesium.crypto.sodium:as s]
     ))
 
-
-(def mykey (sb/new-key!))
-(def plaintext "Hello caesium!")
-(def nonce (sb/int->nonce 0))
-(def ciphertext (sb/encrypt mykey nonce (.getBytes plaintext)))
-(def roundtrip (String. (sb/decrypt mykey nonce ciphertext)))
-(assert (= plaintext roundtrip))
 
 ;; function in crypto.ts
 ; TODO: how can I export this function for external use?
 (defn generateKeys
   (sign/keypair!))
 
+(defn from_base54
+  [string]
+  ;TODO: how to coerce 64 string to byte 
+  (byte-array string)
+  )
+
 ;TODO pick up here
 (defn hashPassword
+  "accepts base64 string or bytearray" 
   [password salt]
-  )
+  (let [salt (if (instance? String salt)
+            (from_base64 salt)
+            salt)]
+    (s/crypto_pwhash
+      s/crypto_box_SEEDBYTES
+      password
+      salt
+      s/crypto_pwhash_OPSLIMIT_INTERACTIVE
+      s/crypto_pwhash_MEMLIMIT_INTERACTIVE
+      s/crypto_pwhash_ALG_DEFAULT)))
+
+(defn maskeSalt)
+
+(defn encrypt)
+
+(defn decrypt)
+
+(defn exportPrivateKey)
+
+(defn importKeyPair)
+
+(defn makeNonc)
+
+(defn rsaEncrypt)
+
+(defn rsaDecrypt)
+
+(s/from_base64 "test")
+
+(instance? (Class/forName "[B") (byte-array [3 5 8]))
+(instance? String "yup")
